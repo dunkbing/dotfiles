03a96f45a04e133eec6f81bc9df03d5044ba9346
binh.buidang <dangbinh4869@gmail.com>
2022-04-08 09:55:24 +0700

    add: nvm.

--- install.sh
+++ install.sh
@@ -54,3 +54,23 @@ else
     echo "Vimplug already installed"
 fi
 # endregion
+
+# region nvm
+# export NVM_DIR="$HOME/.nvm" && (
+#   git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+#   cd "$NVM_DIR"
+#   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+# ) && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+# check if nvm exists and install
+if [ ! -d "$NVM_DIR" ]; then
+    echo "Installing nvm"
+    git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+    cd "$NVM_DIR"
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+    . "$NVM_DIR/nvm.sh"
+else
+    echo "Nvm already installed"
+fi
+# endregion
+
