7aad592ca05ac9d9dc7bedc5f06047aa23eb29b1
binh.buidang <dangbinh4869@gmail.com>
2022-04-08 14:01:13 +0700

    update: logging.

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+function info() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+function warn() {
+    echo -e "\033[33m$1\033[0m"
+}
+
+function error() {
+    echo -e "\033[31m$1\033[0m"
+}
+
+info "Installing dependencies..."
+
 # params: plugin_dir, username, repo
 function install_plugin() {
     if [ -d "$1/$3" ]; then
-        echo "Plugin $2/$3 already installed"
-        echo "Updating..."
+        info "Updating plugin $2/$3..."
         cd "$1/$3" && git pull
     else
         git clone --depth=1 "https://github.com/$2/$3.git" "$1/$3"
@@ -25,10 +38,10 @@ install_plugin $tmux_plugins_dir "tmux-plugins" "tpm"
 function link_file() {
     # check symlink and dir exist
     if [[ ! -L $2 && ! -e $2 ]]; then
-        echo "Linking $1 to $2"
+        info "Linking $1 to $2"
         ln -s $1 $2
     else
-        echo "File or directory already exists(source: $1, target: $2)"
+        warn "File or directory already exists(source: $1, target: $2)"
     fi
 }
 
@@ -47,30 +60,35 @@ link_file $HOME/dotfiles/nvim $HOME/.config/
 vimplug_dir="$HOME/.local/share/nvim/site/autoload/plug.vim"
 vimplug_url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 if [ ! -f "$vimplug_dir" ]; then
-    echo "Installing vimplug"
+    info "Installing vimplug"
     curl -fLo $vimplug_dir --create-dirs $vimplug_url
     nvim '+PlugInstall' '+qa'
 else
-    echo "Vimplug already installed"
+    warn "Vimplug already installed"
 fi
 # endregion
 
 # region nvm
-# export NVM_DIR="$HOME/.nvm" && (
-#   git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-#   cd "$NVM_DIR"
-#   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-# ) && \. "$NVM_DIR/nvm.sh"
 export NVM_DIR="$HOME/.nvm"
 # check if nvm exists and install
 if [ ! -d "$NVM_DIR" ]; then
-    echo "Installing nvm"
+    info "Installing nvm"
     git clone https://github.com/creationix/nvm.git "$NVM_DIR"
     cd "$NVM_DIR"
     git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-    . "$NVM_DIR/nvm.sh"
+    . "$NVM_DIR/install.sh"
 else
-    echo "Nvm already installed"
+    warn "Nvm already installed"
 fi
 # endregion
 
+unset zsh_plugins_dir
+unset zsh_themes_dir
+unset tmux_plugins_dir
+unset install_plugin
+unset link_file
+unset vimplug_dir
+unset vimplug_url
+unset info
+unset warn
+unset error
